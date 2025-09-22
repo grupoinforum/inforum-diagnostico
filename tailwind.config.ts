3285daccf84c7af1c09b9b6f0ab2a05cd7bb4c72
grupoinforum <info@inforumsol.com>
2025-09-22 11:53:43 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
-
-import type { Config } from "tailwindcss"
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
-} satisfies Config
+} satisfies Config;
